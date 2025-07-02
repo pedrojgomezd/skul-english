8005702cdd940e5214a704e5372d34a4d735f06c
Pedro Gomez <pedrojgomez.dev@gmail.com>
2025-07-01 19:43:06 -0500

    Slint seting full 2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
     },
   },
 ];
